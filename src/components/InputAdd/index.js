53061e39ebd63afbdcfa91dd672afa0751516e5e
Ciervos <55422681+Ciervos@users.noreply.github.com>
2020-09-23 20:53:13 -0300

    setinputs

--- src/components/InputAdd/index.js
+++ src/components/InputAdd/index.js
@@ -4,10 +4,11 @@ import {useValid} from '../../hooks/validate';
 
 function InputAdd(props) {
   const [isValid,handleChange] = useValid(props.len);
+  const {name,place,onChange} = props;
 
   return(<div className="inputcont">
-  <label>{props.name}</label>
-  <input type="text" placeholder={props.place} onChange={handleChange}/>
+  <label>{name}</label>
+  <input name={name} type="text" placeholder={place} onKeyDown={handleChange} onChange={onChange}/>
   
   
   {props.len > 0 &&(isValid ? <p>Válido</p> :<p>no válido</p>)}
--- src/pages/Agregar/index.js
+++ src/pages/Agregar/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './style.scss';
 import InputAdd from '../../components/InputAdd';
 import {useValid} from '../../hooks/validate';
@@ -6,16 +6,23 @@ import {useValid} from '../../hooks/validate';
 
 function Agregar() {
   const [quetal] = useValid();
+  const[inputs,setInputs] = useState({});
+
+  function handleInfo(e){
+ const{value,name} = e.target;
+  setInputs({...inputs,[name]:value});
+ 
+  }
 
   return (
 <form>
-<InputAdd name="Nombre" len="2"/>
-<InputAdd name="Logo" len="3"/>
-<InputAdd name="Mail" len="3"/>
+<InputAdd name="Nombre" len="2" onChange={handleInfo}/>
+<InputAdd name="Logo" len="3"onChange={handleInfo}/>
+<InputAdd name="Mail" len="3"onChange={handleInfo}/>
 <textarea/>
-<InputAdd name="Web" len="0" place="opcional"/>
-<InputAdd name="Twitter" len="0" place="opcional"/>
-<InputAdd name="Instagram" len="0" place="opcional"/>
+<InputAdd name="Web" len="0" place="opcional" onChange={handleInfo}/>
+<InputAdd name="Twitter" len="0" place="opcional"onChange={handleInfo}/>
+<InputAdd name="Instagram" len="0" place="opcional" onChange={handleInfo}/>
 <input type="submit" value="Submit"/>
 
 </form>
